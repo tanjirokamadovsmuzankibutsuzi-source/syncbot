d25bccf2b77d1f85ed83acfa795dbaf124ddeb05
tanjirokamadovsmuzankibutsuzi-source <tanjirokamadovsmuzankibutsuzi@gmail.com>
2025-12-28 16:59:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # ⚙️ BOT CONFIGURATION
 
-# Bot Father se mila hua Token
+# Bot Token from BotFather
 BOT_TOKEN = "7212124385:AAGWRwcGVahPwMK7qEfcfviNIGXJ5JepLzc"
 
-# Apki Telegram ID (Taaki koi aur bot use na kare)
-OWNER_ID = 6514133475 
+# Owner ID (Security)
+OWNER_ID = 6514133475
 
-# Worker URL Base (Agar GDrive links convert karne hain)
+# Worker URL Base (Google Drive links ko convert karne ke liye)
 INDEX_WORKER = "https://bypasszbot.legendindex.workers.dev"
 
-# Timeout Settings (Seconds)
-PROCESS_TIMEOUT = 60  # Agar 60 sec tak FFmpeg kuch na kare to kill kar do
+# Max Timeout for FFmpeg (Seconds)
+PROCESS_TIMEOUT = 180 
